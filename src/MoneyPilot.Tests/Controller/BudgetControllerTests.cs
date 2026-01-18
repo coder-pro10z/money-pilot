703a57a65e7ffd8a397429408aa81ec1dc9a0e59
coder-pro10z <2pkashyap2001@gmail.com>
2026-01-19 03:47:21 +0530

    Add BudgetController unit test and remove empty test file
    Removed the unused UnitTest1.cs file. Added BudgetControllerTests.cs, which includes a unit test for the BudgetController's GetAll method using xUnit and Moq to verify correct response and data handling.
    
    Partial test cases.

---
+++ src/MoneyPilot.Tests/Controller/BudgetControllerTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+using MoneyPilot.Application.Interfaces;
+using MoneyPilot.API.Controllers;
+using MoneyPilot.Application.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoneyPilot.Tests.Controller
+{
+    public class BudgetControllerTests
+    {
+        // Add your test methods here
+
+        //for BudgetController
+        [Fact]
+        public async Task GetAllAsyncBudget() {
+            // Arrange
+            // Mock dependencies
+            //services like UserManager, BudgetRepository, etc.
+            //mock service BudgetService
+            var mockBudgetService = new Mock<IBudgetService>();
+            //setup mock methods if needed
+            //using .setup() method of Moq
+            mockBudgetService.Setup(s => s.GetAllAsync("test-user"))
+                .ReturnsAsync(new List<BudgetResponseDto>
+                { new BudgetResponseDto
+                    {
+                        Id = 1,
+                        MonthlyLimit = 500,
+                        Month = new DateTime(2024, 6, 1),
+                        CategoryId = 1,
+                        CategoryName = "Groceries"
+                    }
+                });
+
+            //Controller instantiation         
+            var controller = new BudgetController(mockBudgetService.Object);
+            // Act
+            var result =await controller.GetAll("test-user");
+            // Call the method you want to test here
+            // Assert
+            // Add your assertions here
+            //type of result
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var budgets = Assert.IsAssignableFrom<IEnumerable<BudgetResponseDto>>(okResult.Value);
+            //if single budget returned
+            Assert.Single(budgets);
+        }
+        }
+    }
